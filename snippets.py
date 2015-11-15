150c43194be8fe75583254e0041dfa5979686fa9
hssolo <hansosantos@gmail.com>
2015-11-15 06:13:48 +0000

    prefinal

--- snippets.py
+++ snippets.py
@@ -3,111 +3,118 @@ import argparse
 import sys
 import psycopg2
 
-
+# set log file and log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+# initial connection to PostgresSQL db
 logging.debug("Connecting to PostgreSQL")
 connection = psycopg2.connect("dbname= 'snippets'")
 logging.debug("Database connection established.")
 
-def put(name, snippet):
+
+def put(name, snippet, hide=False):
+    #Question: because the value of arguments is a dictionary without order and used in, put(**arguments),
+    #how is name, snippet and hide able to take the appropriate values from put(**arguments)?
+    #import pdb; pdb.set_trace()
     """Store a snippet with an associated name."""
-    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
-    #NOTE: Allows Python to run sql commands in Postgres session.
-    cursor = connection.cursor()
-    #NOTE: %s is substituted by the tuple in the cursor.execute method.
-    try:
-        command = "insert into snippets values (%s, %s)"
-        cursor.execute(command, (name, snippet))
-    except psycopg2.IntegrityError:
-        print('exception error')
-        connection.rollback()
-        command = "update snippets set message=%s where keyword=%s"
-        cursor.execute(command, (snippet, name))
-    connection.commit()
-    logging.debug("Snippet stored successfully.")
-    return name, snippet
+    logging.info("Storing snippet {!r}: {!r}, {!r}".format(name, snippet, hide))
+    with connection, connection.cursor() as cursor:
+        try:
+            command = "insert into snippets values (%s, %s, %s)"
+            cursor.execute(command, (name, snippet, hide))
+        except psycopg2.IntegrityError:
+            connection.rollback()
+            command = "update snippets set message=%s, hidden=%s where keyword=%s"
+            cursor.execute(command, (snippet, hide, name))
+        connection.commit()
+        logging.debug("Snippet stored successfully.")
+        return name, snippet
     
 def get(name):
-    """Retrieve the snippet with a given name. If there is no such snippet nothing Returns the snippet"""
+    """
+        Retrieve the snippet with a given name. If there is no such snippet 
+        nothing Returns the snippet
+    """
     logging.info("Retrieving snippet {!r})".format(name)) 
-    cursor = connection.cursor()
-    #QUESTION: Dont understand how it knows where to substitute the %s.
-    command = "select keyword, message from snippets where keyword= (%s)"
-    #ANSWER:  psycopg2 does translation of this
-    #NOTE: When you execute a SQL statement, the parameters must always be packaged up into a tuple. This requires a comma: (word,) not just (word).
-    cursor.execute(command, (name,)) 
-    result = cursor.fetchone() 
-    connection.commit()
+    with connection, connection.cursor() as cursor:
+        cursor.execute("select message from snippets where keyword=%s", (name,))
+        result = cursor.fetchone()
+        connection.commit()
     if not result:
         print("that snippet does not exist")
+        print(" -h or --help for additional commands")
     else: 
-        logging.debug("Snippet pulled successfuly: {!r}".format(result[1]))
-        return result[1]
+        logging.debug("Snippet pulled successfuly: {!r}".format(result))
+        return result
+        
+def catalog():
+    """List all the Keyword names in the snippets db"""
+    logging.info("Retrieving keywords")
+    cursor = connection.cursor()
+    command = "select keyword from snippets where hidden=False order by keyword"
+    cursor.execute(command)
+    result = cursor.fetchall() 
+    connection.commit()
+    return result
+    
+def search(string):
+    """Search for a string through all the snippets in the snippets db"""    
+    logging.info("Searching for strings")
+    cursor = connection.cursor()
+    cursor.execute("select * from snippets where message like {!r} and hidden=False".format(string))
+    result = cursor.fetchall()
+    connection.commit()
+    logging.debug("String found successfuly: {!r}".format(result))
+    return result
+    
     
 def main():
-    #NOTE: Alone this only provides the description without any other help.
     """Main function"""
     logging.info("Constructing parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
-    print('printing argparse object:')
-    print(parser)
-    print("\n")
+    subparsers = parser.add_subparsers(dest="can_be_anything", help="Available commands")
     
-    # #NOTE: Allows for positional arguments. Alone it creates an {empty set} of available commands.
-    logging.info("Constructing subparser")
-    subparsers = parser.add_subparsers(dest="command", help="Available commands") 
-    print('printing subparse object:')
-    print(subparsers)
-    print("\n")
-    
-    #NOTE: Provides values to {empty set} in subparsers.
-    logging.debug("Constructing put_subparser")
+    # Subparser for put command
+    logging.debug("Constructing put subparsers")
     put_parser = subparsers.add_parser("put", help="Store a snippet")
-    print(put_parser)
-    print("\n")
-    
-    #NOTE: Provides values to {empty set} in subparsers object.
-    logging.debug("Constructing get_subparser")
-    get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
-    print(get_parser)
-    print("\n")
-    
-    #NOTE: Adds positional arguments to --help
-    put_parser.add_argument("name", help="The name of the snippet") 
+    put_parser.add_argument("name", help="The name of the snippet")#'name' is variable for  what is being expected  as a command line argument.  
     put_parser.add_argument("snippet", help="The snippet text") 
-    get_parser.add_argument("name", help="The name of the snippet") #Positional Argument
+    put_parser.add_argument("--hide", action="store_true")
     
-    #NOTE: This becomes Namespace
-    arguments = parser.parse_args(sys.argv[1:]) 
-    print('First Value: The "arguments" variable equals "Namespace" of the command line arguments')
-    print(arguments)
-    print (type(arguments))
-    print("\n")
+    # Subparser for get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
+    get_parser.add_argument("name", help="The name of the snippet")
     
-    #NOTE: Converts the command line arguments of variable-"arguments" into a dictionary
+    # Subparser for catalog command
+    logging.debug("Constructing catalog subparser")
+    catalog_parser = subparsers.add_parser("catalog", help="Query for keywords")
+    
+    # Subparser for search command
+    logging.debug("Constructing search subparser")
+    search_parser = subparsers.add_parser("search", help="Run a search")
+    search_parser.add_argument("string", help="search string in snippet")
+
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = parser.parse_args(sys.argv[1:])
     arguments = vars(arguments)
-    print('Second Value: The arguments variable is converted to a string based key:value dictionary')
-    print(arguments)
-    print(type(arguments))
-    print(len(arguments))
-    print("\n")
-    
-    #NOTE:"command" string in parentheses relates to dest=("command") of subparser on line 40.
-    kommand = arguments.pop("command") 
-    print('the value of kommand is:')
-    print(kommand)
-    print("\n")
+    kommand = arguments.pop("can_be_anything")
+
     
     if kommand == "put":
-        another_name, another_snippet = put(**arguments)
-        print(another_snippet)
-        print(another_name)
+        another_name, another_snippet = put(**arguments) 
+        #Question: Why can't I use "hide" here? I get an unpack error.
+        print(another_name, another_snippet)
         print("Stored {!r} as {!r}".format(another_snippet, another_name))
     elif kommand == "get":
         message = get(**arguments)
-        print(message)
         print("Retrieved snippet: {!r}".format(message))
-   
+    elif kommand == "catalog":
+        keywords = catalog()
+        print("Retrieved list of names: {!r}".format(keywords)) 
+    elif kommand == "search":
+        find = search(**arguments)
+        print("Search found the following: {!r}".format(find))
 
     
 if __name__ == "__main__":
@@ -116,6 +123,9 @@ if __name__ == "__main__":
 
 
 
-  #!r = used for print and logging info such as used with .format, %s is used with variables 
+#Question: Again, When do I use {!r} vs %s?
+#Question: How would I have used --show or --unhide?
+
+
  
   
